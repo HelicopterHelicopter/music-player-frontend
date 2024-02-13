489753ce04e2093c93e51a12531c1280dca54474
c209362 <jheel.rathod@iifl.com>
2024-02-13 10:28:17 +0530

    youtube changes

--- src/pages/YoutubePlayer.tsx
+++ src/pages/YoutubePlayer.tsx
@@ -1,9 +1,53 @@
+import { useState } from "react";
 import ReactPlayer from "react-player";
+import { getYtPresignedUrl, searchYT } from "../utils/api-communicator";
 
 const YoutubePlayer = () => {
+
+    //const [videoId,setVideoId] = useState<string>("");
+    //const [videoUrl,setVideoUrl] = useState<string>("");
+    const [searchKeyword,setSearchKeyword] = useState<string>("");
+    const [results,setSearchResults] = useState<any[]>([]);
+
+    // const onVideoIdSubmit = async()=>{
+    //     const url = await getYtPresignedUrl(videoId);
+    //     if(url.message==="OK"){
+    //         setVideoUrl(url.body);
+    //     }
+    // }
+
+    // const onVideoIdSubmit2 = () => {
+    //     window.open(`music.jheel.org:5000/api/v1/yt/play?videoid=${videoId}`);
+    // }
+
+    const onSearchClick =async () => {
+        const data =  await searchYT(searchKeyword);
+        if(data.message==="OK"){
+            setSearchResults(data.data);
+        }
+    }
     return(
         <div>
-            <ReactPlayer url="http://13.127.64.232:5000/api/v1/yt/play?url=https://www.youtube.com/watch?v=oMZHqoZojZ0WKWDng2H3tc"/>
+            <label htmlFor="keyword">Search:</label>
+            <input type="text" onChange={(e)=>setSearchKeyword(e.target.value)} name="keyword" id="keyword"/>
+            <button type="button" onClick={onSearchClick}>Submit</button>
+            <table className="table">
+                <thead>
+                    <th>Title</th>
+                    <th>URL</th>
+                </thead>
+                <tbody>
+                    {results.map((result)=>(
+                        <tr>
+                            <td>{result.title}</td>
+                            <td>{result.url}</td>
+                        </tr>
+                        
+                        
+                    ))}
+                </tbody>
+            </table>
+            
         </div>
     );
 }
--- src/utils/api-communicator.tsx
+++ src/utils/api-communicator.tsx
@@ -13,6 +13,28 @@ export const getBucketSongs = async() => {
     return data;
 }
 
+export const getYtPresignedUrl = async (videoId:string) => {
+    const res = await axios.get(`music.jheel.org:5000/api/v1/yt/getYtURL?videoId=${videoId}`);
+
+    if(res.status!=200){
+        throw new Error("Unable to get yt presigned URL");
+    }
+
+    const data = await res.data;
+    return data;
+}
+
+export const searchYT = async (searchKeyword:string) => {
+    const res = await axios.get(`https://gl5sbklz21.execute-api.ap-south-1.amazonaws.com/UAT/api/v1/yt/search?searchKeyword=${searchKeyword}`);
+
+    if(res.status!=200){
+        throw new Error("Unable to get search Results");
+    }
+
+    const data = await res.data;
+    return data;
+}
+
 export const generatePresignedUrl = async (key:string) => {
     const res = await axios.post("https://k7l85e2xf3.execute-api.ap-south-1.amazonaws.com/UAT/music-player-backend",{
         command:"PRESIGNED_URL",
@@ -26,6 +48,5 @@ export const generatePresignedUrl = async (key:string) => {
     const data = await res.data;
 
     return data;
-
 
 }
